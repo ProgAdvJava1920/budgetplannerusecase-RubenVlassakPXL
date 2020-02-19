bd69ef7c55efce5f7b0d6009c3d319797e93498e
Ruben Vlassak <ruben.vlassak@student.pxl.be>
2020-02-19 10:36:06 +0100

    Added functionality to read file and changed log4j properties file

--- src/main/java/be/pxl/student/BudgetPlanner.java
+++ src/main/java/be/pxl/student/BudgetPlanner.java
@@ -1,6 +1,11 @@
 package be.pxl.student;
 
+import be.pxl.student.util.BudgetPlannerImporter;
+
+import java.nio.file.Paths;
+
 public class BudgetPlanner {
-
-
+    public static void main(String[] args) {
+        new BudgetPlannerImporter().importCsv(Paths.get("src/main/resources/account_payments.txt"));
+    }
 }
--- src/main/java/be/pxl/student/util/BudgetPlannerImporter.java
+++ src/main/java/be/pxl/student/util/BudgetPlannerImporter.java
@@ -3,16 +3,35 @@ package be.pxl.student.util;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.nio.file.Path;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.*;
 
 /**
  * Util class to import csv file
  */
 public class BudgetPlannerImporter {
     private static final Logger LOGGER = LogManager.getLogger(BudgetPlannerImporter.class);
+    private PathMatcher csvMatcher = FileSystems.getDefault().getPathMatcher("glob:**/*.csv");
 
     public void importCsv(Path path) {
         // test of het bestand een CSV is
-
+        if (!csvMatcher.matches(path)) {
+            // accolades wordt vervangen door de parameter
+            LOGGER.debug("Invalid file: .csv expected. Provided: {}", path);
+            return;
+        }
+        if (!Files.exists(path)) {
+            LOGGER.error("File {} does not exist.", path);
+        }
+        // properste manier om bufferedreader aan te maken
+        try (BufferedReader reader = Files.newBufferedReader(path)) { // try with resources
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            LOGGER.fatal("An error occurred while reading file: {}", path);
+        }
     }
 }
