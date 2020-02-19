10cd1c38ca1076df5472e1b64bc487f951e24b7b
Ruben Vlassak <ruben.vlassak@student.pxl.be>
2020-02-19 09:34:05 +0100

    Changes to pom.xml and basic changes to classes Payment and BudgetPlannerImporter

--- src/main/java/be/pxl/student/util/BudgetPlannerImporter.java
+++ src/main/java/be/pxl/student/util/BudgetPlannerImporter.java
@@ -1,7 +1,18 @@
 package be.pxl.student.util;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.nio.file.Path;
+
 /**
  * Util class to import csv file
  */
 public class BudgetPlannerImporter {
+    private static final Logger LOGGER = LogManager.getLogger(BudgetPlannerImporter.class);
+
+    public void importCsv(Path path) {
+        // test of het bestand een CSV is
+
+    }
 }
